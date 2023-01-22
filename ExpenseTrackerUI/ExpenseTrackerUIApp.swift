b10bc87f54e4a59860e79dbc3a89eaabbaffbfdd
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2023-01-22 15:48:29 +0400

    Initial Commit

---
+++ ExpenseTrackerUI/ExpenseTrackerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerUIApp.swift
+//  ExpenseTrackerUI
+//
+//  Created by Konstantin Bolgar-Danchenko on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
